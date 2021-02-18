acf8f48662e6dc08c91d0031afbbf3f0d819a12c
Samu Kumpulainen <onesamu@gmail.com>
2021-02-18 20:18:27 +0200

    Start working on logic
    - Add input fields

--- tempo.js
+++ tempo.js
@@ -1,13 +1,30 @@
 const Counter = {
     data() {
       return {
+        time: 3600,
         timer: 0
+
       }
     },
     mounted() {
-        setInterval(() => {
-          this.timer++
-        }, 1000)
+        
+    },
+    computed: {
+        hours() {
+            return this.minutes/60
+        },
+        minutes() {
+            return this.timer/60
+        },
+        seconds() {
+            return this.timer
+        }
+    },
+    methods: {
+        startTimer: () => {
+            console.log("starting timer")>
+           //Start timer here
+        }
     }
   }
   
